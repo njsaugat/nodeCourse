3f6203af71279d7629145dee1af973b4d47f4084
Saugat Poudel <njsaugat@gmail.com>
2022-05-17 07:58:12 +0545

    shop file added

--- express/routes/shop.js
+++ express/routes/shop.js
@@ -5,7 +5,13 @@ const adminData  = require('./admin');
 const rootDirectory=require('../utils/pathFinder')
 router.get('/', (req, res,next) => {
   console.log(adminData.products)
-  res.sendFile(path.join(rootDirectory,'/views/shop.html'))
+  const products=adminData.products;
+  // res.sendFile(path.join(rootDirectory,'/views/shop.html'))//without using templating engine
+  res.render('shop',{products,
+    docTitle:'Shop',
+    hasProducts:products.length>0,
+    activeShop:true,
+    productCSS:true});
 });
 
 module.exports = router;
