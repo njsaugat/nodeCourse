24cedf17a3feac68b0286f7257e1896e835cb6b5
Saugat Poudel <njsaugat@gmail.com>
2022-05-23 14:13:58 +0545

    admin file updated

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -2,21 +2,23 @@ const express=require('express')
 const path=require('path')
 const router=express.Router();//like routing ko lagi special
 const rootDirectory=require('../utils/pathFinder')
+const adminController=require('../controllers/admin');
+const { route } = require('express/lib/application');
+const { rmSync } = require('fs');
+const { runInContext } = require('vm');
+const req = require('express/lib/request');
 
-
-router.get('/',(req,res)=>{
-    // res.sendFile(path.join(rootDirectory,'/views/add-product.html'))without using templating engine
-    res.render('add-product',{docTitle:'Add Product'})
-})
+router.get('/add-product',adminController.getAddProduct)
 // app.post('/message',parser,(req,res)=>{//body-parser package install garera garyo vane function bhitra pass garna pardaina
-const products=[];
-router.post('/add-product',(req,res)=>{
-    // console.log(req.body.title);
-    products.push({title:req.body.title});
-    // console.log(products)
-    // res.setHeader('Location','/');
-    res.redirect('/');//just like mathiko like location '/' --> esto set garya jast
-    // res.send('<h1>data submitted</h1>');
 
-})
-module.exports={router,products};
+router.get('/products',adminController.getProductsAdmin)
+
+router.post('/add-product',adminController.postAddProduct)
+
+router.get('/edit-product/:id',adminController.editProduct)
+
+router.post('/products/:id',adminController.postProduct)
+
+router.post('/delete-product/:id',adminController.deleteProduct)
+// router.get('/products/:id',adminController.editProduct)
+module.exports={router};
