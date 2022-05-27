e172ebb46ff61d465dc6d67467ffb33629001c8d
Saugat Poudel <njsaugat@gmail.com>
2022-05-27 09:54:24 +0545

    shop js file updated

--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const path=require('path');
-const adminData  = require('./admin');
+// const adminData  = require('./admin');
 const rootDirectory=require('../utils/pathFinder')
-router.get('/', (req, res,next) => {
-  console.log(adminData.products)
-  const products=adminData.products;
-  // res.sendFile(path.join(rootDirectory,'/views/shop.html'))//without using templating engine
-  res.render('shop',{products,
-    docTitle:'Shop',
-    hasProducts:products.length>0,
-    activeShop:true,
-    productCSS:true});
-});
+const shopController=require('../controllers/shop');
+const { route } = require("express/lib/application");
+router.get('/',shopController.getIndex);
+
+router.get('/products',shopController.getProducts);
+
+// router.get('/products/delete')
+
+router.get('/products/:id', shopController.getProduct)//dynamic routes has to at lat
+
+router.get('/cart',shopController.getCart);
+
+router.post('/add-to-cart',shopController.postCart)
+
+router.post('/cart-delete-item',shopController.postCartDelete)
+
+router.get('/orders',shopController.getOrder);
+
+router.get('/checkout',shopController.getCheckout)
+
 
 module.exports = router;
