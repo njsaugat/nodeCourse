ce5557136227a569c029ada0af5948517c52c9aa
Saugat Poudel <njsaugat@gmail.com>
2022-06-02 07:29:34 +0545

    admin file added

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -14,33 +14,39 @@ const postAddProduct=(req,res)=>{
     const imageUrl=req.body.imageUrl;
     const price=req.body.price;
     const description=req.body.description; 
-    const product=new Product(title,imageUrl,price,description)//ho esle ho create garne product lai
-    product
-        .save()
-        .then(res.redirect('/') )
-        .catch(err=>console.log(err));
-    ;
+    //previously with only mySql we had to use save() which would save to db
+    //here also we we could use similar approach by using Product.build-->return only data but not save, but by using create method--> save to db
+    Product.create({
+        title,price,imageUrl,description
+    })
+    .then(result=>console.log('Product created'))//kinda like db.execute    
+    .catch(err=>console.log(err))
 
 }
 
 const getProductsAdmin=(req,res,next)=>{
-    Product.fetchAll(products=>{
+    Product.findAll()
+    .then(products=>{
         res.render('admin/products-admin',{
             products,
             hasProducts:products.length>0,
             docTitle:'Admin Products'
         })
     })
+    .catch(err=>console.log(err))
+    
 }
 
 const editProduct=(req,res,next)=>{
-    Product.fetchAll(products=>{
-        const finalProduct=products.filter((product)=>(product.id===req.params.id))[0]
+    Product.findByPk(req.params.id)
+    .then(product=>{
         res.render('admin/edit-product',{
-            finalProduct,
+            finalProduct:product,
             docTitle:'Edit Products',
-        })
+        }) 
     })
+    .catch(err=>console.log(err))
+    
 }
 
 
@@ -53,13 +59,27 @@ const postProduct=(req,res,next)=>{
         description:req.body.description
 
     }
-    Product.update(req.params.id,editObject);
-    res.redirect('/')
+    // Product.findByPk(req.params.id)
+    // .then(product=>{
+    //     if(!product){
+    //         return res.redirect('/');
+    //     }
+    // })
+    Product.update(editObject,
+        {where: {id:req.params.id}}
+    )
+    .then(()=>res.redirect('/'))
+    .catch(err=>console.log(err))
+    
+    
 }
 
 const deleteProduct=(req,res,next)=>{
-    Product.delete(req.params.id);
-    res.redirect('/')
+    Product.destroy({where:{id:req.params.id}})
+    .then(()=>res.redirect('/'))
+    .catch(err=>console.log(err))
+
+    
 }
 
 module.exports={
@@ -71,7 +91,51 @@ module.exports={
     deleteProduct
 }
 
+// depracted mySql code:
+
+// const deleteProduct=(req,res,next)=>{
+//     Product.delete(req.params.id);
+//     res.redirect('/')
+// }
+// const getProductsAdmin=(req,res,next)=>{
+    // Product.fetchAll(products=>{
+    //     res.render('admin/products-admin',{
+    //         products,
+    //         hasProducts:products.length>0,
+    //         docTitle:'Admin Products'
+    //     })
+    // })
+// })
+
+
+// const editProduct=(req,res,next)=>{Product.fetchAll(products=>{
+// const finalProduct=products.filter((product)=>(product.id===req.params.id))[0]
+// res.render('admin/edit-product',{
+//     finalProduct,
+//     docTitle:'Edit Products',
+// })
+// })
+// })
+
+
+
+
+
 // depracted file code:
+
+// const postAddProductMysql=(req,res)=>{
+//     const title=req.body.title;
+//     const imageUrl=req.body.imageUrl;
+//     const price=req.body.price;
+//     const description=req.body.description; 
+//     const product=new Product(title,imageUrl,price,description)//ho esle ho create garne product lai
+//     product
+//         .save()
+//         .then(res.redirect('/') )
+//         .catch(err=>console.log(err));
+    
+
+// }
 // const postAddProductFile=(req,res)=>{
 //     const title=req.body.title;
 //     const imageUrl=req.body.imageUrl;
