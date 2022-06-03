a02f8021100aacc2dcbc14a345f76cac9bb10365
Saugat Poudel <njsaugat@gmail.com>
2022-06-03 07:14:50 +0545

    database file added

--- express/utils/database.js
+++ express/utils/database.js
@@ -1,10 +1,24 @@
-const mysql=require('mysql2');
+const Sequelize=require('sequelize');
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'admin',
-    database:'node_complete',
-    password:'$Saugu2058'
-})
+const sequelize=new Sequelize('node_complete',
+                    'admin',
+                    '$Saugu2058',{
+                        dialect:'mysql',
+                        host:'localhost'
+                    })
 
-module.exports=pool.promise();  
+
+module.exports=sequelize;
+//this util is kinda the place to like to connect to the database
+
+//code to connect to the database without using the orm
+
+// const mysql=require('mysql2');
+// const pool=mysql.createPool({
+//     host:'localhost',
+//     user:'admin',
+//     database:'node_complete',
+//     password:'$Saugu2058'
+// })
+
+// module.exports=pool.promise();  
