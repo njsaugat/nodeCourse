40280680274c044dd6eb124fa210bdd9453969b0
Saugat Poudel <njsaugat@gmail.com>
2022-06-04 14:23:38 +0545

    shop file updated

--- express/controllers/shop.js
+++ express/controllers/shop.js
@@ -1,10 +1,13 @@
 // const adminData  = require('./admin')
-const { Cart } = require('../models/cart');
-const {Product}=require('../models/productData')
+const Cart = require('../models/cart');
+const Product=require('../models/productData')
 let products=require('../data/productsData.json')
-
+const Order=require('../models/order')
+const CartItem=require('../models/cartItem')
 const getProducts=(req, res,next) => {
-    Product.findAll()//built in method in sequelize
+    // Product.findAll()//built in method in sequelize
+    req.user
+    .getProducts()
     .then((products)=>{
         res.render('shop/product-list',{
             products:products,
@@ -47,44 +50,118 @@ const getIndex=(req,res,next)=>{
 }
 
 const getCart=(req,res,next)=>{
-    Cart.getCart(cart=>{
-        Product.fetchAll(products=>{
-            const cartProducts=[]
-            for(let product of products){
-                const cardProductData=cart.products.find(prod=>prod.id===product.id)
-                if(cardProductData){
-                    cartProducts.push({productData: product,qty:cardProductData.qty});
-                }
-                res.render('shop/cart',{
-                    docTitle:'Your Cart',
-                    products:cartProducts
-                })
-            }
-           
+    req.user
+        .getCart()
+        .then(cart=>{
+            return cart.getProducts();
         })
-    })
+        .then(cartProducts=>{
+            res.render('shop/cart',{
+                docTitle:'Your Cart',
+                products:cartProducts
+            })
+
+        }) 
+        .catch(err=>console.log(err))
     
 }
 
-const postCart=async (req,res,next)=>{
-    const id=req.body.id// add to cart lai form bana bhaera tesma input tag hiiden,tyaha input tag ko ko name id thyo ra tesko value ma id thyo;which is already set
-    const product=await Product.findById(id)
-    Cart.addProduct(id,product.price);
-    res.redirect('/cart')
-    // res.render('')
+const postCart= (req,res,next)=>{
+    const prodId=req.params.id;
+    let fetchedCart;
+    let newQuantity=1;
+    req.user
+        .getCart()
+        .then(cart=>{
+            fetchedCart=cart;
+            return cart.getProducts({where:{id:prodId}})
+        })
+        .then(products=>{
+            let product;
+            if(products.length>0){
+                product=products[0];
+            }
+            
+            if(product){
+                const oldQuantity=product.cartItem.quantity;
+                newQuantity=oldQuantity+1;
+                return product;
+            }
+            return Product.findByPk(prodId)
+                    // .then(product=>{
+                    //     return fetchedCart.addProduct(product, {
+                    //         through:{quantity:newQuantity}
+                    //     })
+                    // })
+                    // .catch(err=>console.log(err));
+            
+        })
+        .then(product=>{
+            return fetchedCart.addProduct(product, {
+                through:{quantity:newQuantity}
+            })
+        })
+        .then(()=>{
+            res.redirect('/')
+        })
+        .catch(err=>console.log(err))
 
 }
 
-const postCartDelete=async(req,res,next)=>{
+const postCartDelete=(req,res,next)=>{
     const productId=req.body.productId;
-    const product=await Product.findById(productId)
-    Cart.deleteProduct(productId,product.price)
-    res.redirect('/cart')
+    req.user
+        .getCart()
+        .then(cart=>{
+            return cart.getProducts({where:{id:productId}})
+        })
+        .then(products=>{
+            const product=products[0];
+            return product.cartItem.destroy();
+        }).then(result=>{
+            res.redirect('/cart') 
+        })
+        .catch(err=>console.log(err))
+   
 }
 const getOrder=(req,res,next)=>{
-    res.render('shop/orders',{
-        docTitle:'Your Order'
+    req.user.getOrders({include:['products']})
+    .then(orders=>{
+        res.render('shop/orders',{
+            orders,
+            docTitle:'Your Order'
+        })
     })
+    .catch(err=>console.log(err))
+    
+}
+
+const postOrder=(req,res,next)=>{
+    let fetchedCart;
+    req.user
+        .getCart()
+        .then(cart=>{
+            fetchedCart=cart;
+            return cart.getProducts()
+        })
+        .then(products=>{
+            return req.user.createOrder();
+        })
+        .then(order=>{
+            order.addProducts(products.map(product=>{
+                product.orderItem={quantity:product.cartItem.quantity }
+                return product;
+            }))
+        })
+        .then(result=>{
+            return fetchedCart.setProducts(null);
+        })
+        .then(result=>{
+            res.redirect('/orders')
+
+        })
+        
+        .catch(err=>console.log(err))
 }
 const getCheckout=(req,res,next)=>{
     res.render('shop/checkout',{
@@ -101,7 +178,8 @@ module.exports={
     postCartDelete,
     getCart,
     postCart,
-    getOrder
+    getOrder,
+    postOrder
 }
 
 // depracated code for mysql
@@ -133,7 +211,35 @@ module.exports={
     // })
     // .catch((err)=>(console.log(err)))
     // }
+// depracted file code:
+// const getCart=(req,res,next)=>{
+//     Cart.getCart(cart=>{
+//         Product.fetchAll(products=>{
+//             const cartProducts=[]
+//             for(let product of products){
+//                 const cardProductData=cart.products.find(prod=>prod.id===product.id)
+//                 if(cardProductData){
+//                     cartProducts.push({productData: product,qty:cardProductData.qty});
+//                 }
+//                 res.render('shop/cart',{
+//                     docTitle:'Your Cart',
+//                     products:cartProducts
+//                 })
+//             }
+           
+//         })
+//     })
+    
+// }
 
+// const postCart=async (req,res,next)=>{
+//     const id=req.body.id// add to cart lai form bana bhaera tesma input tag hiiden,tyaha input tag ko ko name id thyo ra tesko value ma id thyo;which is already set
+//     const product=await Product.findById(id)
+//     Cart.addProduct(id,product.price);
+//     res.redirect('/cart')
+//     // res.render('')
+
+// }
 
 
 
