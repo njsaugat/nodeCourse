f3cf4d785362e6c8fb35a3df133a66e2f38f9ff1
Saugat Poudel <njsaugat@gmail.com>
2022-06-04 14:23:54 +0545

    product data file updated

--- express/models/productData.js
+++ express/models/productData.js
@@ -1,28 +1,55 @@
-const db=require('../utils/database');
+const Sequelize=require('sequelize');
 
-const Cart=require('./cart')
-class Product{
-    constructor(title,imageUrl,price,description,){
-        this.title=title;
-        this.imageUrl=imageUrl;
-        this.price=price;
-        this.description=description;
-    }
-    save(){
-        return db.execute(`INSERT INTO products(title,price,description,imageUrl) 
-                    VALUES (?,?,?,?)`, //? is added to add values later pass as array
-        [this.title,this.price,this.description, this.imageUrl])
-    }
+const sequelize =require('../utils/database');
 
-    // static update(id,editObject){}
-    // static delete(id){}
+const Product=sequelize.define('product',{
+        id:{
+            type:Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey:true
+        },
+        title:Sequelize.STRING,
+        price:{
+            type:Sequelize.STRING,
+            allowNull:false
+        },
+        imageUrl:{
+            type:Sequelize.STRING,
+            allowNull:false
+        },
+        description:{
+            type:Sequelize.STRING,
+            allowNull:false
+        }
+})
 
-    static fetchAll(){
-        return db.execute('SELECT * FROM products')
-    }
-    static findById(id){
-        return db.execute(`SELECT * FROM products
-                    WHERE products.id=? `,[id])
-    }
-}
-module.exports={Product}
+module.exports=Product;
+// const db=require('../utils/database');
+
+// const Cart=require('./cart')
+// class Product{
+//     constructor(title,imageUrl,price,description,){
+//         this.title=title;
+//         this.imageUrl=imageUrl;
+//         this.price=price;
+//         this.description=description;
+//     }
+//     save(){
+//         return db.execute(`INSERT INTO products(title,price,description,imageUrl) 
+//                     VALUES (?,?,?,?)`, //? is added to add values later pass as array
+//         [this.title,this.price,this.description, this.imageUrl])
+//     }
+
+//     // static update(id,editObject){}
+//     // static delete(id){}
+
+//     static fetchAll(){
+//         return db.execute('SELECT * FROM products')
+//     }
+//     static findById(id){
+//         return db.execute(`SELECT * FROM products
+//                     WHERE products.id=? `,[id])
+//     }
+// }
+// module.exports={Product}
